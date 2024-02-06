6d13b345a5c9c5e498d3e32af6b5322255d6164d
Dodam0719 <blueoh719@gmail.com>
2024-02-06 14:52:04 +0900

    Feat: 주사위 기록 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ function random(n) {
 function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -32,6 +35,8 @@ function App() {
         <Dice color="blue" num={num} />
         <h2>총점</h2>
         <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
       </div>
     </div>
   );
